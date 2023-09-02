6393607a576d7f3cb13325cf9c1745fd0639b80d
AndriiKlyman <59160757+AndriiKlyman@users.noreply.github.com>
2023-09-02 22:08:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,25 @@ import numpy as np
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+st.line_chart(chart_data)
+
 st.button("Reset1", type="primary")
 st.button("Reset2", type="secondary")
 st.button("Reset3", type="secondary")
 st.button("Reset4", type="secondary")
 st.button("Reset5", type="secondary")
-
-st.line_chart(chart_data)
