125fce449e4e9892516b8b2732b686b876bbaeea
AndriiKlyman <59160757+AndriiKlyman@users.noreply.github.com>
2023-09-02 22:05:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,10 @@ import numpy as np
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
+st.button("Reset1", type="primary")
+st.button("Reset2", type="secondary")
+st.button("Reset3", type="secondary")
+st.button("Reset4", type="secondary")
+st.button("Reset5", type="secondary")
 
 st.line_chart(chart_data)
